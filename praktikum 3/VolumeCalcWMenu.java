2704dada6e0c74a4785c41d4cfa9f38d86a681b0
mashmarsreal <142289058+mashmarsreal@users.noreply.github.com>
2025-10-15 16:58:55 +0700

    Update and rename PraktikumLoop3.java to VolumeCalcWMenu.java

--- praktikum 3/VolumeCalcWMenu.java
+++ praktikum 3/VolumeCalcWMenu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PraktikumLoop3 {
+public class VolumeCalcWMenu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int pil,w,x,y,z;
@@ -75,3 +75,4 @@ public class PraktikumLoop3 {
         System.out.println("Program dihentikan...");
     }
 }
+
